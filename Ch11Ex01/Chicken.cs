46f50e25c6f54c69b7c2d1c943a743e8fa184798
nodormu <60243248+nodormu@users.noreply.github.com>
2020-06-17 12:05:50 -0500

    Add files via upload

---
+++ Ch11Ex01/Chicken.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch11Ex01
+{
+    public class Chicken : Animal  //Animal.name does NOT work. You must inherit, because it's OOP. In the first several chapters, I was able to reference things this way, but only public.
+    {
+        public void LayEgg() => WriteLine($"{name} has laid an egg.");  // You can't do Animal.name because of it's protection level, it can only be inherited via the class because it's OOP.
+        public Chicken(string newName) : base(newName) { }
+    }
+}
---
+++ Ch11Ex01/Cow.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Ch11Ex01
+{
+    public class Cow : Animal  //Animal.name does NOT work. You must inherit. because its OOP. In the first several chapters I was able to reference things, but only public.
+    {
+        public void Milk() => WriteLine($"{name} has been milked.");  // You can't do Animal.name because of it's protection level, it can only be inherited via the class becuase its OOP.
+        public Cow(string newName) : base(newName) { } // See line 32 in Main for this instanciation:  Cow myCow2 = new Cow("Donna");
+    }
+}
